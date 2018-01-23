48e190b68f5811a20085af7c1307688907b5a607
AnacondaY <oliver_young@qq.com>
2018-01-23 22:40:49 +0800

    modify unit test

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -19,14 +19,14 @@ describe('=== error import declaration ===', () => {
         const code = 'import * as chimera from \'chimera-ui\'';
         expect(() => babel(code)).toThrowError(
             'The namespaced import declaration like: import * as chimera from \'chimera-ui\' is not supported'
-        )
+        );
     });
 
     it('throw error on default import declaration', () => {
         const code = 'import chimera from \'chimera-ui\'';
         expect(() => babel(code)).toThrowError(
             'The default import declaration like: import chimera from \'chimera-ui\' is not supported'
-        )
+        );
     });
 
 });
@@ -37,24 +37,24 @@ describe('=== transform import declaration correctly ===', () => {
 
     it('with default options', () => {
         const result = babel(code);
-        expect(result.includes('import Button from \'chimera-ui/lib/Button\'')).toBeTruthy();
-        expect(result.includes('import \'chimera-ui/lib/Button/style/index.scss\'')).toBeTruthy();
+        expect(result.includes('import Button from \'chimera-ui/lib/components/Button\'')).toBeTruthy();
+        expect(result.includes('import \'chimera-ui/lib/components/Button/style/index.scss\'')).toBeTruthy();
     });
 
     it('with css style', () => {
         const result = babel(code, { style: 'css' });
-        expect(result.includes('import Button from \'chimera-ui/lib/Button\'')).toBeTruthy();
-        expect(result.includes('import \'chimera-ui/lib/Button/style/css/index.css\'')).toBeTruthy();
+        expect(result.includes('import Button from \'chimera-ui/lib/components/Button\'')).toBeTruthy();
+        expect(result.includes('import \'chimera-ui/lib/components/Button/style/css/index.css\'')).toBeTruthy();
     });
 
     it('without style', () => {
         const result = babel(code, { style: false });
-        expect(result.includes('import Button from \'chimera-ui/lib/Button\'')).toBeTruthy();
-        expect(result.includes('import \'chimera-ui/lib/Button/style/index.scss\'')).toBeFalsy();
+        expect(result.includes('import Button from \'chimera-ui/lib/components/Button\'')).toBeTruthy();
+        expect(result.includes('import \'chimera-ui/lib/components/Button/style/index.scss\'')).toBeFalsy();
     });
 
     it('modify libDirectory', () => {
-        expect(babel(code, { libDirectory: 'xxx' }).includes('import Button from \'chimera-ui/xxx/Button\'')).toBeTruthy();
+        expect(babel(code, { libDirectory: 'xxx' }).includes('import Button from \'chimera-ui/xxx/components/Button\'')).toBeTruthy();
     });
 
 });
